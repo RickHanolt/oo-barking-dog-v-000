d71f1ad39ac36ab3d4d429d0d52197da1602cbf0
Richard Hanolt <rhanolt@gmail.com>
2020-07-05 09:43:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,3 @@
-# Your code goes here!
 require 'pry'
 
-class Dog
 
-  def initialize(bark = "woof!")
-    @name = name
-    @bark = bark
-  end
-
-  def name
-    @name
-  end
-
-  def bark
-    puts "woof!"
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
-
-  binding.pry
-end
